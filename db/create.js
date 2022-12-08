9125884c1fe285115d3456b2a847365faa968932
cusmoritz <cus.moritz@gmail.com>
2022-12-08 16:24:22 -0700

    chore: update links list

--- db/create.js
+++ db/create.js
@@ -9,16 +9,16 @@ const client = new Client(DATABASE_URL);
 // then after we parse them, send each 'item' to the database
 const FEED_LINKS = [
     {name: 'Ars Technica', link: 'https://feeds.arstechnica.com/arstechnica/staff-blogs'}, // works with parseURL
-    {name: 'NotSure', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCN-v-Xn9S7oYk0X2v1jx1Qg'}, // works with parseURL
+    {name: 'StoneMountain64', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCN-v-Xn9S7oYk0X2v1jx1Qg'}, // works with parseURL
     {name: 'The Verge', link: 'https://www.theverge.com/rss/index.xml'}, // works with parseURL
     {name: 'Corey Doctrow', link: 'https://pluralistic.net/feed/'}, // works with parseURL
     {name: 'WIRED Latest Ideas', link: 'https://www.wired.com/feed/category/ideas/latest/rss'}, // works with parseURL
-    {name: 'NotSure2', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCZy67OUMYggYSGDyYAviFUg'},
-    {name: 'NotSure3', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCRHXUZ0BxbkU2MYZgsuFgkQ'},
+    {name: 'Vail Daily', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCZy67OUMYggYSGDyYAviFUg'},
+    {name: 'SpiffingBrit', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCRHXUZ0BxbkU2MYZgsuFgkQ'},
     {name: 'ed zitron', link: 'https://ez.substack.com/feed'},
     {name: 'Garbage Day', link: 'https://www.garbageday.email/feed'},
     {name: 'everything is amazing', link: 'https://everythingisamazing.substack.com/feed'},
-    {name: 'stonemountain', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCw7FkXsC00lH2v2yB5LQoYA'},
+    {name: 'JackFrags', link: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCw7FkXsC00lH2v2yB5LQoYA'},
     {name: 'xkcd', link: 'https://xkcd.com/rss.xml'},    
 ];
 
