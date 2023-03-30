779c6a94d9931d133f690d504f58be6b50c2a4f0
cusmoritz <cus.moritz@gmail.com>
2023-03-30 16:47:49 -0600

    feat(search): displaying search results on front end, needs styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,24 +6,36 @@ import { searchPosts } from '../api';
 const SearchBar = ({links, user}) => {
 
     const [search, setSearch] = useState("");
+    const [results, setResults] = useState();
+    const [display, setDisplay] = useState();
 
     const userSearchPosts = async () => {
         console.log('this was search: ', search);
         const searchResults = await searchPosts(search, user);
-        // console.log('search results front: ', searchResults)
+        console.log('search results front: ', searchResults)
         // this might be an array if i did it right
         // make the call to the api here to find a post with the search term
         // using the users id (user)
         // and the search term
-        
+        setResults(searchResults);
+        setNested();
+        return searchResults;
     };
 
     const handleSearch = (searchTerm) => {
         setSearch(searchTerm);
-        if (search != "") {
+        if (search !== "") {
             userSearchPosts();
         }
     }
+
+    const setNested = () => {
+        for (let i=0;i<results.length; i++) {
+            setDisplay(results[i]);
+        }
+        console.log('display', display);
+    }
+
 
     return (
         <div className="search-container">
@@ -33,6 +45,17 @@ const SearchBar = ({links, user}) => {
                     value={search} 
                     onChange={(event) => handleSearch(event.target.value) }></input>
             </form>
+            {!display
+            ? null
+            : display.map((post) => {
+                console.log('posts????', post)
+                return(
+                    <div className="search-results-container">
+                        <div key={post.id} className="one-result">{post.title}</div>
+                    </div>
+                )
+            })
+            }
         </div>
 
     )
